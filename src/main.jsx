3513df71d080cd3423daad8ec09c7e10354dc46b
Shravan Sulikeri <ssuliker@uncc.edu>
2025-11-23 20:56:10 -0500

    chore: add Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Portfolio from './Portfolio.jsx';
 import './index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 const rootElement = document.getElementById('root');
 
@@ -9,6 +10,7 @@ if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <Portfolio />
+      <Analytics />
     </React.StrictMode>
   );
 }
